fa7f9a882b26586e8899e6e6920c54ac0ad9c2ad
nynex97 <nynexxx666@gmail.com>
2024-11-09 18:33:15 +0300

    neutral

--- game/defendersimba/scripts/vscripts/tables/neutral_camps.lua
+++ game/defendersimba/scripts/vscripts/tables/neutral_camps.lua
@@ -1,30 +1,32 @@
 NEUTRAL_CAMPS = {
     [1] = {
-        {{unit = "npc_dota_neutral_1", count = 2}, {unit = "npc_dota_neutral_2", count = 2}},
-        {{unit = "npc_dota_neutral_3", count = 1}},
-        {{unit = "npc_dota_neutral_1", count = 3}, {unit = "npc_dota_neutral_3", count = 1}},
+        {{unit = "npc_dota_neutral_1_1", count = 2}, {unit = "npc_dota_neutral_1_2", count = 2}},
+        {{unit = "npc_dota_neutral_1_3", count = 1}},
+        {{unit = "npc_dota_neutral_1_1", count = 3}, {unit = "npc_dota_neutral_1_3", count = 1}},
     },
     [2] = {
-        {{unit = "npc_dota_neutral_4", count = 1}},
+        {{unit = "npc_dota_neutral_2_1", count = 2}, {unit = "npc_dota_neutral_2_2", count = 2}},
+        {{unit = "npc_dota_neutral_2_3", count = 1}},
+        {{unit = "npc_dota_neutral_2_1", count = 3}, {unit = "npc_dota_neutral_2_3", count = 1}},
     },
     [3] = {
-        {{unit = "npc_dota_neutral_1", count = 2}, {unit = "npc_dota_neutral_2", count = 2}},
-        {{unit = "npc_dota_neutral_3", count = 1}},
-        {{unit = "npc_dota_neutral_1", count = 3}, {unit = "npc_dota_neutral_3", count = 1}},
+        {{unit = "npc_dota_neutral_3_1", count = 2}, {unit = "npc_dota_neutral_3_2", count = 2}},
+        {{unit = "npc_dota_neutral_3_3", count = 1}},
+        {{unit = "npc_dota_neutral_3_1", count = 3}, {unit = "npc_dota_neutral_3_3", count = 1}},
     },
     [4] = {
-        {{unit = "npc_dota_neutral_1", count = 2}, {unit = "npc_dota_neutral_2", count = 2}},
-        {{unit = "npc_dota_neutral_3", count = 1}},
-        {{unit = "npc_dota_neutral_1", count = 3}, {unit = "npc_dota_neutral_3", count = 1}},
+        {{unit = "npc_dota_neutral_4_1", count = 2}, {unit = "npc_dota_neutral_4_2", count = 2}},
+        {{unit = "npc_dota_neutral_4_3", count = 1}},
+        {{unit = "npc_dota_neutral_4_1", count = 3}, {unit = "npc_dota_neutral_4_3", count = 1}},
     },
     [5] = {
-        {{unit = "npc_dota_neutral_1", count = 2}, {unit = "npc_dota_neutral_2", count = 2}},
-        {{unit = "npc_dota_neutral_3", count = 1}},
-        {{unit = "npc_dota_neutral_1", count = 3}, {unit = "npc_dota_neutral_3", count = 1}},
+        {{unit = "npc_dota_neutral_5_1", count = 2}, {unit = "npc_dota_neutral_5_2", count = 2}},
+        {{unit = "npc_dota_neutral_5_3", count = 1}},
+        {{unit = "npc_dota_neutral_5_1", count = 3}, {unit = "npc_dota_neutral_5_3", count = 1}},
     },
     [6] = {
-        {{unit = "npc_dota_neutral_1", count = 2}, {unit = "npc_dota_neutral_2", count = 2}},
-        {{unit = "npc_dota_neutral_3", count = 1}},
-        {{unit = "npc_dota_neutral_1", count = 3}, {unit = "npc_dota_neutral_3", count = 1}},
+        {{unit = "npc_dota_neutral_6_1", count = 2}, {unit = "npc_dota_neutral_6_2", count = 2}},
+        {{unit = "npc_dota_neutral_6_3", count = 1}},
+        {{unit = "npc_dota_neutral_6_1", count = 3}, {unit = "npc_dota_neutral_6_3", count = 1}},
     },
 }
